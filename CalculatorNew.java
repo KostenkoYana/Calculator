3c8fcc8191af372cab3a8bf1d02348383c5664a4
Yana <E230508701@st.atlas.edu.tr>
2023-11-05 17:42:58 +0300

    Added subtraction method

--- CalculatorNew.java
+++ CalculatorNew.java
@@ -3,9 +3,12 @@ public class Calculator() {
 		return a + b;
 	}
 	public double addition(double a, double b) {
-		return a + b;
-	}
-	public double addition(double a, double b) {
 		return b + a;
+	}
+	public int subtraction(int a, int b) {
+		return a - b;
+	}
+	public double subtraction(double a, double b) {
+		return b - a;
 	}
 }
